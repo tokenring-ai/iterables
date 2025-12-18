673f1190e0aa8267bc6c93f466c5ec6f97a1d3e6
Mark Dierolf <mdierolf@cloudquote.io>
2025-12-18 13:53:39 -0800

    Extract and modularize plugin logic into `plugin.ts`

--- index.ts
+++ index.ts
@@ -1,21 +1,2 @@
-import {AgentCommandService} from "@tokenring-ai/agent";
-import TokenRingApp from "@tokenring-ai/app";
-import {TokenRingPlugin} from "@tokenring-ai/app";
-import chatCommands from "./chatCommands.ts";
-import IterableService from "./IterableService.js";
-import packageJSON from './package.json' with {type: 'json'};
-
-export default {
-  name: packageJSON.name,
-  version: packageJSON.version,
-  description: packageJSON.description,
-  install(app: TokenRingApp) {
-    app.waitForService(AgentCommandService, agentCommandService =>
-      agentCommandService.addAgentCommands(chatCommands)
-    );
-    app.addServices(new IterableService());
-  },
-} as TokenRingPlugin;
-
 export {default as IterableService} from "./IterableService.ts";
 export type {IterableProvider, IterableItem, IterableSpec, IterableMetadata} from "./IterableProvider.ts";
---
+++ plugin.ts
@@ -0,0 +1,19 @@
+import {AgentCommandService} from "@tokenring-ai/agent";
+import TokenRingApp from "@tokenring-ai/app";
+import {TokenRingPlugin} from "@tokenring-ai/app";
+import chatCommands from "./chatCommands.ts";
+import IterableService from "./IterableService.js";
+import packageJSON from './package.json' with {type: 'json'};
+
+
+export default {
+  name: packageJSON.name,
+  version: packageJSON.version,
+  description: packageJSON.description,
+  install(app: TokenRingApp) {
+    app.waitForService(AgentCommandService, agentCommandService =>
+      agentCommandService.addAgentCommands(chatCommands)
+    );
+    app.addServices(new IterableService());
+  },
+} as TokenRingPlugin;
