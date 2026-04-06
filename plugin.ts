184a033c115e995500860bc9193eaf90428f99df
Mark Dierolf <mdierolf@cloudquote.io>
2026-04-06 14:50:21 -0700

    Update plugin metadata, schemas, and refine descriptions

--- plugin.ts
+++ plugin.ts
@@ -9,6 +9,7 @@ const packageConfigSchema = z.object({});
 
 export default {
   name: packageJSON.name,
+  displayName: "Batch Iterables",
   version: packageJSON.version,
   description: packageJSON.description,
   install(app, config) {
